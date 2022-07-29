dce0858a2535bd2dbdc849bec99cb1e8d9ea7182
jahid-22 <lionhunterjahid@gmail.com>
2022-07-29 06:12:29 +0600

    kicho change

---
+++ js/own.js
@@ -0,0 +1,50 @@
+// progress bar designe
+let html = document.querySelector('#html')
+
+setTimeout(() =>{
+    html.style.width = html.getAttribute('data-done') + '%';
+    html.style.opacity = '1'
+},1400)
+
+let css = document.querySelector('#css')
+
+setTimeout(() =>{
+    css.style.width = css.getAttribute('data-done') + '%';
+    css.style.opacity = '1'
+},1400)
+
+let b5 = document.querySelector('#b5')
+
+setTimeout(() =>{
+    b5.style.width = b5.getAttribute('data-done') + '%';
+    b5.style.opacity = '1'
+},1400)
+
+let js = document.querySelector('#js')
+
+setTimeout(() =>{
+    js.style.width = js.getAttribute('data-done') + '%';
+    js.style.opacity = '1'
+},1400)
+
+let responsive = document.querySelector('#responsive')
+
+setTimeout(() =>{
+    responsive.style.width = responsive.getAttribute('data-done') + '%';
+    responsive.style.opacity = '1'
+},1400)
+
+let python = document.querySelector('#python')
+
+setTimeout(() =>{
+    python.style.width = python.getAttribute('data-done') + '%';
+    python.style.opacity = '1'
+},1400)
+
+let django = document.querySelector('#django')
+
+setTimeout(() =>{
+    django.style.width = django.getAttribute('data-done') + '%';
+    django.style.opacity = '1'
+},1400)
+
